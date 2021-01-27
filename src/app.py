ebafbb90414fd7cb8951ea29f2ca45ac0751d3bd
Zilfa Cyamani <zilcyam@gmail.com>
2021-01-27 14:30:22 +0200

    lat correction

--- src/app.py
+++ src/app.py
@@ -1,17 +1,8 @@
 import sys
 import os
 
-def palindrome(string):
-	left_pos = 0
-	right_pos = len(string) - 1
-	
-	while right_pos >= left_pos:
-		if not string[left_pos] == string[right_pos]:
-			return False
-		left_pos += 1
-		right_pos -= 1
-	return True
-print(palindrome('radar')) 
+def palindrome(s):
+	return s[::-1] == s
 
 def solution(s):
     return palindrome(s)
